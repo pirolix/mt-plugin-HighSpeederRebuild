de9bb13b697fea6b0de9688923e97ada78072807
pirolix <pirolix@adbff654-c4ea-448a-8ae3-cad037306009>
2012-10-20 08:01:26 +0000

    * バージョン番号を埋め込み
    git-svn-id: svn://svn.magicvox.net/var/svn/mt-plugins/HighSpeederRebuild@247 adbff654-c4ea-448a-8ae3-cad037306009

--- HighSpeederRebuild.pl
+++ HighSpeederRebuild.pl
@@ -1,12 +1,14 @@
 package MT::Plugin::OMV::HighSpeederRebuild;
+# $Id$
 
 use strict;
 use MT 3;
 use MT::Entry;
 
-use vars qw( $MYNAME $VERSION );
-$MYNAME = 'HighSpeederRebuild';
-$VERSION = '0.01 DEVEL';
+use vars qw( $VENDOR $MYNAME $VERSION );
+($VENDOR, $MYNAME) = (split /::/, __PACKAGE__)[-2, -1];
+(my $revision = '$Rev$') =~ s/\D//g;
+$VERSION = '0.01'. ($revision ? ".$revision" : '');
 
 use base qw( MT::Plugin );
 my $plugin = __PACKAGE__->new({
